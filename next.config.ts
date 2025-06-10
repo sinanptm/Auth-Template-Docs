944c70e0ba9cb111e097396a9b7033c9a1d49121
muhammed sinan <muhammedsinan7749@gmail.com>
2025-06-11 00:48:01 +0530

    first commit

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "github.com",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com"
+    ],
+  },
+};
+
+export default nextConfig;
