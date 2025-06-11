01b222abce2d870868e891e3573a81a31266eb16
muhammed sinan <muhammedsinan7749@gmail.com>
2025-06-11 11:48:39 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { mainFont } from "@/lib/fonts";
 import { WrapperProps } from "@/types";
 import "@/styles/globals.css";
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { Suspense } from "react";
 
 export { metadata } from "./metadata";
 
@@ -9,7 +10,11 @@ const RootLayout = ({ children }: WrapperProps) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${mainFont.className} antialiased `}>
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <NuqsAdapter>
+          <Suspense>
+            {children}
+          </Suspense>
+        </NuqsAdapter>
       </body>
     </html>
   );
